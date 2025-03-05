87db6f365135f21609260966397ddff935339170
HelenaAlamp <159056949+HelenaAlamp@users.noreply.github.com>
2025-03-05 15:59:13 -0500

    Update JuTrack.jl

--- src/JuTrack.jl
+++ src/JuTrack.jl
@@ -84,6 +84,13 @@ export matrix_to_array, array_to_matrix
 export total_length, spos, findelem, insert_space_charge, array_optics, get_len
 export ADfindm66_refpts
 
+#added
+export SC_lens
+export SC_kick, track_SC!
+export track!
+export TransferMap4D, Inverse_TransferMap4D, TransferMap4DChrom
+
+
 function Duplicated(x::Float64, dx::Base.RefValue{Float64})
     return Duplicated(x, dx[])
 end
