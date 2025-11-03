d1c9c9800cc54e6d2574aeb440c834119886f523
Jinyu Wan <wanjinyu8@gmail.com>
2025-11-03 13:42:54 -0500

    CI test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,9 @@
-# in Julia REPL, run ] to enter package mode
+# in Julia REPL, type ] to enter package mode
 # activate JuTrack
 # test JuTrack
 
 using JuTrack
 using Test
-using Enzyme
 
 function f(x)
     k1 = x[1]
@@ -22,7 +21,7 @@ ctps2 = CTPS(3.0, 1, 6, 3)
 findindex(ctps2, [1, 0, 0, 0, 0, 0, 0])
 ind = findindex(ctps2, [1, 0, 0, 0, 0, 0, 0])
 ctps3 = ctps2*ctps2
-ctps4 = inv(ctps2)
+ctps4 = 1.0 / ctps2
 ctps5 = exp(ctps2)
 ctps6 = log(ctps2)
 ctps7 = sqrt(ctps2)
@@ -52,7 +51,31 @@ k1 = 3.0
 k2 = 1.5
 y = f([k1, k2])
 println(y)
-# grad = jacobian(Forward, f, [k1, k2])
-# println(grad)
+grad = jacobian(Forward, f, [k1, k2])
+println(grad)
 
+# particle tracking
+beam = Beam([0.001 0.0 0.0 0.0 0.0 0.0])
+D1 = DRIFT(len=1.0)
+Q1 = QUAD(k1=1.2, len=0.5)
+Q2 = QUAD(k1=-1.2, len=0.5)
+line = [D1, Q1, D1, Q2, D1]
+linepass!(line, beam)
+println(beam.r)
+
+# fast tpsa
+set_tps_dim(6)
+x = DTPSAD(0.001, 1)
+xp = DTPSAD(0.0, 2)
+y = DTPSAD(0.0, 3)
+yp = DTPSAD(0.0, 4)
+z = DTPSAD(0.0, 5)
+dp = DTPSAD(0.0, 6)
+beam_tpsa = Beam([x xp y yp z dp])
+D1 = DRIFT(len=DTPSAD(1.0))
+Q1 = QUAD(k1=DTPSAD(1.2), len=DTPSAD(0.5))
+Q2 = QUAD(k1=DTPSAD(-1.2), len=DTPSAD(0.5))
+line_ = [D1, Q1, D1, Q2, D1]
+linepass!(line_, beam_tpsa)
+println(beam_tpsa.r)
 end
